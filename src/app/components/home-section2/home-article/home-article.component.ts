0c8bf56963fd69401aeb6b2858b4a360fd5f4737
mariamamr439-bit <mariamamr439@gmail.com>
2026-03-25 07:48:05 +0200

    fixx'

--- src/app/components/home-section2/home-article/home-article.component.ts
+++ src/app/components/home-section2/home-article/home-article.component.ts
@@ -1,9 +1,10 @@
 import { Component,Input } from '@angular/core';
 import { blogData } from '../../../interfaces/blog-data';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-article',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-article.component.html',
   styleUrl: './home-article.component.css',
 })
